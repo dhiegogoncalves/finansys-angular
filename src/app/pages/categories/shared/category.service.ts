8ffacd76c6dd878f60169e5f31c9b878720e99e5
Dhiego <dhhiego@gmail.com>
2020-04-11 13:38:08 -0400

    Implementação do category form

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -36,7 +36,7 @@ export class CategoryService {
 
   update(category: Category): Observable<Category> {
     const url = `${this.apiPath}/${category.id}`;
-    return this.http.put(this.apiPath, category).pipe(
+    return this.http.put(url, category).pipe(
       catchError(this.handleError),
       map(() => category)
     );
