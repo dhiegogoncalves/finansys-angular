b3abb9affd27013046244250c015073f0161f2d2
Dhiego <dhhiego@gmail.com>
2020-04-23 18:23:29 -0400

    Refatoração do Page Header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent],
+  declarations: [BreadCrumbComponent, PageHeaderComponent],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
     BreadCrumbComponent,
+    PageHeaderComponent,
   ],
 })
 export class SharedModule {}
