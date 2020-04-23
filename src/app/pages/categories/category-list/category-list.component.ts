55fd0d75b5aaa04a0cefc9b399102275e2324761
Dhiego <dhhiego@gmail.com>
2020-04-23 10:50:07 -0400

    Refatoração do Category e Entry List

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,34 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BaseResourceListComponent } from '../../../shared/components/base-resource-list/base-resource-list.component';
 
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
-import { element } from 'protractor';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css'],
 })
-export class CategoryListComponent implements OnInit {
-  categories: Category[] = [];
-
-  constructor(private categoryService: CategoryService) {}
-
-  ngOnInit(): void {
-    this.categoryService.getAll().subscribe(
-      (categories) => (this.categories = categories),
-      (error) => console.log('Erro ao carregar a lista')
-    );
-  }
-
-  deleteCategory(category: Category) {
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => (this.categories = this.categories.filter((e) => e !== category)),
-        () => console.log('Erro ao tentar excluir')
-      );
-    }
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
+  constructor(protected categoryService: CategoryService) {
+    super(categoryService);
   }
 }
---
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -0,0 +1,29 @@
+import { OnInit } from '@angular/core';
+
+import { BaseResourceModel } from '../../models/base-resource.model';
+import { BaseResourceService } from '../../services/base-resource.service';
+
+export class BaseResourceListComponent<T extends BaseResourceModel>
+  implements OnInit {
+  resources: T[] = [];
+
+  constructor(protected resourceService: BaseResourceService<T>) {}
+
+  ngOnInit(): void {
+    this.resourceService.getAll().subscribe(
+      (resources) => (this.resources = resources),
+      (error) => console.log('Erro ao carregar a lista')
+    );
+  }
+
+  protected deleteResource(resource: T) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.resourceService.delete(resource.id).subscribe(
+        () => (this.resources = this.resources.filter((e) => e !== resource)),
+        () => console.log('Erro ao tentar excluir')
+      );
+    }
+  }
+}
